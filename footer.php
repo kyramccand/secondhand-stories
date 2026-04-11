6cfcc9168ac785d9abe226d562c8f18d00735a88
eharty01 <155921233+eharty01@users.noreply.github.com>
2026-04-11 13:41:47 -0400

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-<!-- footer.php -->
-<footer>
-    <div>2026 Secondhand Stories. All rights reserved.</div>
-    <div>
-        <a href="privacyPolicy.php">Privacy Policy</a> |
-        <a href="termsOfService.php">Terms of Service</a>
-    </div>
-</footer>
