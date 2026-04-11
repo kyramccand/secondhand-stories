bc914d58381a577ff20843e00185007475ecfd7e
eharty01 <155921233+eharty01@users.noreply.github.com>
2026-04-11 13:34:46 -0400

    Create footer.php (footer php component)

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!-- footer.php -->
+<footer>
+    <div>2026 Secondhand Stories. All rights reserved.</div>
+    <div>
+        <a href="privacyPolicy.php">Privacy Policy</a> |
+        <a href="termsOfService.php">Terms of Service</a>
+    </div>
+</footer>
